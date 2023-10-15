d531721abb76cc62f5a52d19beb896583e31bf41
Aniurm <534147085@qq.com>
2023-10-15 10:11:09 +0800

    refactor: remove useless stuff

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,17 +1,12 @@
 package router
 
 import (
-	"xlab-feishu-robot/internal/controller"
 	"xlab-feishu-robot/internal/dispatcher"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Register(r *gin.Engine) {
-	// register your controllers here
-	// example
-	r.POST("/api/example", controller.Example)
-
 	// DO NOT CHANGE LINES BELOW
 	// register dispatcher
 	r.POST("/feiShu/Event", dispatcher.Dispatcher)
