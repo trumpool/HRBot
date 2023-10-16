00581471867872b5f5e9393e7a58fd0d409574f5
Aniurm <534147085@qq.com>
2023-10-16 18:25:45 +0800

    test: SearchEmployee

--- internal/controller/add_people_test.go
+++ internal/controller/add_people_test.go
@@ -1,12 +1,16 @@
 package controller
 
 import (
+	"context"
+	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 	"xlab-feishu-robot/internal/config"
 	"xlab-feishu-robot/internal/log"
+	"xlab-feishu-robot/internal/pkg"
 )
 
 func Test_getAllPeopleInDepartment(t *testing.T) {
@@ -51,6 +55,36 @@ func Test_inviteUserToGroupChat(t *testing.T) {
 	setupForTest()
 	_, err := inviteUserToGroupChat([]string{"123"}, []string{"123"})
 	assert.NoError(t, err)
+}
+
+func Test_SearchEmployee(t *testing.T) {
+	setupForTest()
+
+	tenantAccessToken, err := GetTenantAccessToken()
+	assert.NoError(t, err)
+	logrus.Info(tenantAccessToken)
+
+	resp, err := pkg.Client.Do(context.Background(),
+		&larkcore.ApiReq{
+			HttpMethod: http.MethodPost,
+			ApiPath:    "https://open.feishu.cn/open-apis/corehr/v2/employees/search",
+			Body: map[string]interface{}{
+				"key_word": "牛马",
+			},
+			QueryParams: larkcore.QueryParams{
+				"page_size":    []string{"10"},
+				"user_id_type": []string{"open_id"},
+			},
+			SupportedAccessTokenTypes: []larkcore.AccessTokenType{larkcore.AccessTokenTypeTenant},
+		},
+	)
+
+	if err != nil {
+		logrus.Errorf("failed to search employee, err: %v", err)
+		return
+	}
+
+	logrus.Info(resp)
 }
 
 func setupForTest() {
