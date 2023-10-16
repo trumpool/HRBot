b985edf780a0ac17f7eea7361e71a5d44abaeb2e
Aniurm <534147085@qq.com>
2023-10-16 15:05:20 +0800

    refactor: more help info

--- internal/controller/help_p2p.go
+++ internal/controller/help_p2p.go
@@ -14,6 +14,9 @@ func HelpP2P(messageEvent *store.MessageEvent) {
 
 	功能1: 批量加人
 		用法："批量加人. 张三, 李四, 王五. 答疑群, 水群, 正式群"
+		注意事项：
+			1. 机器人使用"."和","分割输入文本，标点符号为英文标点。
+			2. 要想拉人进某个群，必须先把机器人拉进该群。
 	`
 	msgContent := map[string]interface{}{
 		"text": helpMessage,
